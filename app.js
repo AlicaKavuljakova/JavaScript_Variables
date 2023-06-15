b57ca02547a17461381960d7a10faf4db192ba26
alica kavuljakova <drobeek@gmail.com>
2023-06-15 14:02:03 -0700

    homework

--- app.js
+++ app.js
@@ -4,7 +4,35 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName="Pippin Peregrin";
+let lastName="Took";
+var age=50;
+lastName="Brandybuck";
+age=49;
+console.log(`${firstName} was a little hobbit.`)
 // Exercise 2
-
+let myFirstName="Alica";
+lastName="Kavuljakova";
+age=38;
 // Exercise 3
+let language="JavaScript";
+let createdYear=1995;
+let isCaseSensitive=true;
+console.log(`${language} was created in ${createdYear}, and is ${isCaseSensitive}ly case sensitive`)
+//Exercise 4
+let productName="t-shirt"
+let price=19.99;
+let isOnSale=false;
+let selePercentage=15;
+let stock=0;
+let inStock=false;
+let selectSize="M";
+console.log(`${productName.toUpperCase()} costs ${price}, is on sale: ${isOnSale}.`)
+console.log(` we have ${stock} in stock`)
+//Exercise 5
+let title="Name of the Wind";
+let author="Patrick Rothfuss";
+let pageCount=722;
+let bookmark=456;
+let hasRead=true;
+console.log(`Title:${title}, by ${author} has ${pageCount} pages`)
